07f59734d56258948a1070c0a39f6249407d268a
sarapiette <spiette@thoughtworks.com>
2016-11-28 19:40:01 -0500

    refactored promises to chain then commands

--- js/exercises/giphy/giphy.js
+++ js/exercises/giphy/giphy.js
@@ -10,10 +10,9 @@ class Giphy {
         this.ui.searchInput = this.ui.element.find('input');
         this.ui.send = this.ui.element.find('button');
 
-        this.ui.search = this.ui.element.find('.search');
-        this.ui.random = this.ui.element.find('.random');
-        this.ui.anything = this.ui.element.find('.anything');
-
+        // this.ui.search = this.ui.element.find('.search');
+        // this.ui.random = this.ui.element.find('.random');
+        // this.ui.anything = this.ui.element.find('.anything');
 
         this.init();
     }
@@ -22,50 +21,119 @@ class Giphy {
         const self = this;
         this.ui.send.on('click', function(event) {
             event.preventDefault();
+            self.getGiphySearchData(self.ui.searchInput.val())
+                .then(self.extractGiphyData)
+                .then(self.showSearchGiphy);
+            self.getGiphyRandomData()
+                .then(self.extractRandomGiphyData)
+                .then(self.showRandomGiphy);
+            self.getGiphyTrendingData()
+                .then(self.extractGiphyData)
+                .then(self.showTrendingGiphy);
 
+            // $.ajax({
+            //    url: 'http://api.giphy.com/v1/gifs/search',
+            //     data: {
+            //        q: self.ui.searchInput.val(),
+            //         limit: 1,
+            //         api_key: 'dc6zaTOxFJmzC'
+            //     },
 
+            //     success: function(data) {
 
-            $.ajax({
-               url: 'http://api.giphy.com/v1/gifs/search',
-                data: {
-                   q: self.ui.searchInput.val(),
-                    limit: 1,
-                    api_key: 'dc6zaTOxFJmzC'
-                },
+            //         self.ui.search.attr('src', data.data[0].images.original.url );
 
-                success: function(data) {
+            //         $.ajax({
+            //             url: 'http://api.giphy.com/v1/gifs/random',
+            //             data: {
+            //                 api_key: 'dc6zaTOxFJmzC',
+            //                 limit: 1
+            //             },
+            //             success: function(data) {
 
-                    self.ui.search.attr('src', data.data[0].images.original.url );
+            //                 self.ui.random.attr('src', data.data['image_url']);
 
-                    $.ajax({
-                        url: 'http://api.giphy.com/v1/gifs/random',
-                        data: {
-                            api_key: 'dc6zaTOxFJmzC',
-                            limit: 1
-                        },
-                        success: function(data) {
+            //                 $.ajax({
+            //                     url: 'http://api.giphy.com/v1/gifs/trending',
+            //                     data: {
+            //                         limit: 1,
+            //                         api_key: 'dc6zaTOxFJmzC',
+            //                     },
+            //                     success: function(data) {
+            //                         self.ui.anything.attr('src', data.data[0].images.original.url);
+            //                     }
+            //                 });
+            //             }
+            //         });
 
-                            self.ui.random.attr('src', data.data['image_url']);
+            //     }
+            // });
+  
+  });
 
-                            $.ajax({
-                                url: 'http://api.giphy.com/v1/gifs/trending',
-                                data: {
-                                    limit: 1,
-                                    api_key: 'dc6zaTOxFJmzC',
-                                },
-                                success: function(data) {
-                                    self.ui.anything.attr('src', data.data[0].images.original.url);
-                                }
-                            });
-                        }
-                    });
+}
 
-                }
-            });
+  getGiphySearchData(input) {
+      const promise = $.ajax({
+          url: 'http://api.giphy.com/v1/gifs/search',
+          data: {
+            q: input,
+            api_key: 'dc6zaTOxFJmzC',
+            limit: 1
+          }
+      });
+      return promise;
+  }
 
-        });
-    }
+  getGiphyRandomData() {
+    const promise = $.ajax({
+          url: 'http://api.giphy.com/v1/gifs/random',
+          data: {
+            api_key: 'dc6zaTOxFJmzC',
+            limit: 1
+          }
+      });
+    return promise;
+  }
 
+  getGiphyTrendingData() {
+    const promise = $.ajax({
+          url: 'http://api.giphy.com/v1/gifs/trending',
+          data: {
+            api_key: 'dc6zaTOxFJmzC',
+            limit: 1
+          }
+      });
+    return promise;
+  }
+  extractGiphyData(data){
+    if(!data.data && ! data.data[0]) {
+            throw 'Entry does not exist in giphy';
+        }
+        return data.data[0].images.original.url;
+  }
+
+  extractRandomGiphyData(data){
+    if(!data.data) {
+            throw 'Entry does not exist in giphy';
+        }
+        return data.data['image_url'];
+  }
+
+  showSearchGiphy(giphyEntry){
+    const $search = $('.search');
+    $search.attr('src', giphyEntry);
+  }
+
+  showRandomGiphy(giphyEntry){
+    const $random = $('.random');
+    $random.attr('src', giphyEntry);
+  }
+
+   showTrendingGiphy(giphyEntry){
+    const $trending = $('.anything');
+    $trending.attr('src', giphyEntry);
+  }
 
 }
 
